0ad7d36f75465407c183282cc375422e323b70fd
jeffch19 <17jcummings@gmail.com>
2024-02-16 17:59:10 -0500

    added image and about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
         <section id="about" className="active">
             <h2>About Me</h2>
             <img src={me} alt="Developer's Photo" className='header-image'/> {/* Use the imported image */}
-            <p>Enthusiastic and aspiring Software Engineer with a strong foundation in web development technologies and a passion for creating dynamic, user-friendly web applications. Eager to contribute my skills and dedication to a collaborative development team. A quick learner with a commitment to staying updated with the latest industry trends and best practices. Excited to take on new challenges and grow as a developer while delivering high-quality web solutions.</p>
+            <p className='p-text'>Enthusiastic and aspiring Software Engineer with a strong foundation in web development technologies and a passion for creating dynamic, user-friendly web applications. Eager to contribute my skills and dedication to a collaborative development team. A quick learner with a commitment to staying updated with the latest industry trends and best practices. Excited to take on new challenges and grow as a developer while delivering high-quality web solutions.</p>
         </section>
         </div>
     );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <ul>
             <li><a href="https://github.com/jeffch19"><img src="/github.jpg" alt="GitHub" className="footer-image" /></a></li>
 <li><a href="https://www.linkedin.com/in/jeff-cummings-782a24170/"><img src="/linkedin.png" alt="LinkedIn" className="footer-image" /></a></li>
-                <li><a href="link-to-third-platform-profile" target="_blank">Third Platform</a></li>
+                
             </ul>
         </footer>
     );
