49a9e6e07d615866735970bc2391ab95abb2fe67
jeffch19 <17jcummings@gmail.com>
2024-02-16 08:28:44 -0500

    added to resume.js

---
+++ src/components/Resume.jsx
@@ -0,0 +1,19 @@
+// Resume.js
+import React from 'react';
+import resume from '../assets/resume.pdf';
+
+const Resume = () => {
+    return (
+        <section id="resume">
+            <h2>Resume</h2>
+            <a href={resume} target="_blank">Download Resume</a>
+            <ul>
+                <li>Front-End Proficiencies</li>
+                <li>Back-End Proficiencies </li>
+                {/* Add more proficiencies */}
+            </ul>
+        </section>
+    );
+};
+
+export default Resume;
