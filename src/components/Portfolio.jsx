1c60ba43738d23ae0eb03a9b0ac1de4460243eb3
jeffch19 <17jcummings@gmail.com>
2024-02-17 18:34:51 -0500

    adding to resume page

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,6 @@
 // Portfolio.js
 import React from 'react';
-// import project1 from '../assets/project1.jpg';
+import project1 from '../assets/linkedin.png';
 // import project2 from '../assets/project2.jpg';
 // import project3 from '../assets/project3.jpg';
 // import project4 from '../assets/project4.jpg';
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,19 +1,35 @@
 // Resume.js
 import React from 'react';
+import './Resume.css';
 
-
-// import resume from '../assets/resume.pdf'; 
+import project1 from '../assets/linkedin.png';
 
 const Resume = () => {
     return (
         <div className='main-content'>
             <section id="resume">
                 <h2>Resume</h2>
-                <a href={resume} target="_blank" rel="noopener noreferrer">Download Resume</a> {/* Add rel attribute for security */}
+                Download my <a href="https://docs.google.com/document/d/1gwar_dsS5eUSnXEu0okDd9-La1OrL_BcdqXn9ckzphk/edit?usp=sharing" target="_blank" rel="noopener noreferrer">resume</a>
+                <h4>Front-End Proficiencies</h4>
                 <ul>
-                    <li>Front-End Proficiencies</li>
-                    <li>Back-End Proficiencies </li>
-                    {/* Add more proficiencies */}
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>jQuery</li>
+                    <li>responsive design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+                <h4>Back-End Proficiencies</h4>
+                <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+
                 </ul>
             </section>
         </div> 
