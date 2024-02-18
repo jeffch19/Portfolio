9aa7c3eb118da242ba50601520717bf56eb414c0
jeffch19 <17jcummings@gmail.com>
2024-02-18 16:30:50 -0500

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 // Footer.js
 import React from 'react';
 import './Footer.css';
+import imgGit from '../assets/github.jpg';
+import imgLinkedIn from '../assets/linkedin.png'; // Import the LinkedIn image
 
 const Footer = () => {
     return (
         <footer className='footer'>
             <ul>
-            <li><a href="https://github.com/jeffch19"><img src="/github.jpg" alt="GitHub" className="footer-image" /></a></li>
-<li><a href="https://www.linkedin.com/in/jeff-cummings-782a24170/"><img src="/linkedin.png" alt="LinkedIn" className="footer-image" /></a></li>
-                
+                <li><a href="https://github.com/jeffch19" target="_blank" rel="noopener noreferrer"><img src={imgGit} alt="GitHub" className="footer-image" /></a></li>
+                <li><a href="https://www.linkedin.com/in/jeff-cummings-782a24170/" target="_blank" rel="noopener noreferrer"><img src={imgLinkedIn} alt="LinkedIn" className="footer-image" /></a></li>
             </ul>
         </footer>
     );
