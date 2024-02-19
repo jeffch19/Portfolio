8eb36276c133efbc0f5aafb0a69bca9577dcb310
jeffch19 <17jcummings@gmail.com>
2024-02-19 16:30:04 -0500

    adding to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './Portfolio.css';
 import project1 from '../assets/portfolioimg1.jpg';
 import imgGit from '../assets/github.jpg';
+import weather1 from '../assets/weather1.jpg';
+import note1 from '../assets/note1.jpg';
 // import project2 from '../assets/project2.jpg';
 // import project3 from '../assets/project3.jpg';
 // import project4 from '../assets/project4.jpg';
@@ -12,25 +14,25 @@ import imgGit from '../assets/github.jpg';
 const Portfolio = () => {
     const projects = [
         {
-          title: 'Project  1',
-          description: 'Description for Project  1',
-          image: project1,
-          deployedLink: 'https://project1.com',
-          githubLink: 'https://github.com/user/project1'
+          title: 'Weather Dashboard',
+          description: 'Server-Side API',
+          image: weather1,
+          deployedLink: 'https://jeffch19.github.io/Weather-Dashboard/',
+          githubLink: 'https://github.com/jeffch19/Weather-Dashboard?tab=readme-ov-file'
         },
         {
-            title: 'Project  1',
-            description: 'Description for Project  1',
-            image: project1,
-            deployedLink: 'https://project1.com',
-            githubLink: 'https://github.com/user/project1'
+            title: 'Note Taker',
+            description: 'Express.js',
+            image: note1,
+            deployedLink: 'https://note-taker19-c47eace3c6ed.herokuapp.com/',
+            githubLink: 'https://github.com/jeffch19/note-taker?tab=readme-ov-file'
           },
           {
-            title: 'Project  1',
-            description: 'Description for Project  1',
+            title: 'Text Editor',
+            description: 'Progressive Web App',
             image: project1,
-            deployedLink: 'https://project1.com',
-            githubLink: 'https://github.com/user/project1'
+            deployedLink: 'https://text-editor-5u8z.onrender.com',
+            githubLink: 'https://github.com/jeffch19/Text-Editor'
           },
           {
             title: 'Project  1',
