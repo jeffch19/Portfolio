3da21bb7ab88148790b9492c5c2fe057b1013f47
jeffch19 <17jcummings@gmail.com>
2024-04-20 21:24:24 -0400

    adding aws cert and updated resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ const Resume = () => {
         <div className='main-content'>
             <section id="resume">
                 <h2>Resume</h2>
-                Download my <a href="https://docs.google.com/document/d/1gwar_dsS5eUSnXEu0okDd9-La1OrL_BcdqXn9ckzphk/edit?usp=sharing" target="_blank" rel="noopener noreferrer">resume</a>
+                Download my <a href="https://docs.google.com/document/d/1gwar_dsS5eUSnXEu0okDd9-La1OrL_BcdqXn9ckzphk/edit?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a> OR view my <a href="https://www.credly.com/badges/ec2a5969-e79e-4d0d-acfa-9e6d00604d5d/email" target="_blank" rel="noopener noreferrer">AWS Certificate</a>
                 <h4>Front-End Proficiencies</h4>
                 <ul>
                     <li>HTML</li>
